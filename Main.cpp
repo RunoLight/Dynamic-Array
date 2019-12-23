2f5185e9e1a04988db590cdd7ac6c2189aa19e9e
Runo <31756050+RunoLight@users.noreply.github.com>
2019-12-23 10:50:00 +1000

    deleted unnecessary

--- Main.cpp
+++ Main.cpp
@@ -5,36 +5,8 @@
 #include <stdlib.h>
 using namespace std;
 
-template <typename TT, class Arr> //6.1
-int find_first(TT val, Arr& arr, int size) //returns index of first value "val" in array "array"
-{
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] == val)
-            return i;
-    }
-    return -1;
-}
-
 int main()
 {
-    // Задание: 6.1 - функция которая будет искать первое вхождение элемента в массиве
-    //          6.2 - класс, хранящий данные, умеющий сортировать и умеющий выводить информацию(info(); )
-    //          8.1 - итератор
-    //          8.2 - аллокатор
-    int iArr[10] = { 2, 5, 7, 15, 60, 80, 90, 10, 10, 0 };
-    float fArr[5] = { 2.4f, 3.14f, 2.0f };
-    bool bArr[5];
-    Dynarray<int> myVec(5);
-    myVec.push_back(1); myVec.push_back(13); myVec.push_back(3);
-
-    //работа 6.1
-    cout << find_first(80, iArr, 10) << endl
-        << find_first(6, fArr, 5) << endl
-        << find_first(true, bArr, 5) << endl
-        << find_first<int, Dynarray<int>>(1, myVec, myVec.size()) << endl;
-
-    //работа 6.2
     myVec.info();
     myVec.sort();
     myVec.info();
@@ -49,7 +21,7 @@ int main()
     dataVec.info();
     dataVec.sort();
     
-    //работа 8.1
+    //Г°Г ГЎГ®ГІГ  8.1
     cout << "\n\n";
     for (Dynarray<SomeData>::iterator it = dataVec.begin(); it != dataVec.end(); it++)
     {
